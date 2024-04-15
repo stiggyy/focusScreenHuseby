60ca6dc51f1efaf8381eb7fb4a736dcaebe69ec0
c huseby <catherine_huseby@outlook.com>
2024-04-15 09:59:40 -0500

    hello

---
+++ focusScreenHuseby/ViewControllerClasses.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerClasses.swift
+//  focusScreenHuseby
+//
+//  Created by CATHERINE HUSEBY on 3/8/24.
+//
+
+import UIKit
+
+class ViewControllerClasses: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ focusScreenHuseby/ViewControllerCreate.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerCreate.swift
+//  focusScreenHuseby
+//
+//  Created by CATHERINE HUSEBY on 3/7/24.
+//
+
+import UIKit
+
+class ViewControllerCreate: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
